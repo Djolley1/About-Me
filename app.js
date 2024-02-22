203e58e947d6193dc421885e0920e79d1b9f3d75
Demarquies Jolley <ahmir1128@gmail.com>
2024-02-21 18:29:09 -0600

    added JS files

--- app.js
+++ app.js
@@ -1,56 +1,93 @@
 // Asking Name
 function getName() {
-    let name = prompt("What is your name?");
-    document.write("Nice to meet you " + name);
-  }
-  getName()
+  let name = prompt("What is your name?");
+  document.write("Nice to meet you " + name);
+}
 
-  // Sports Question
-  function getSport() {
-    let choice = prompt("Do you like sports?");
-    let answer = choice.charAt(0).toLowerCase();
-    if( answer === "y" ) {
-      document.write(" Cool, I'm an athlete also. ");
-    } else {
-      document.write(" That's totally fine... ");
-    }
+// Sports Question
+function getSport() {
+  let choice = prompt("Do you like sports?");
+  let answer = choice.charAt(0).toLowerCase();
+  if( answer === "y" ) {
+    document.write(" Cool, I'm an athlete also. ");
+  } else {
+    document.write(" That's totally fine... ");
   }
-  getSport()
+}
 
 
 // Getting Age
 function getAge() {
-    let Age = prompt("Are you over 30?");
-    let answer = Age.charAt(0).toLowerCase();
-    if( answer === "y" ) {
-      document.write(" I bet your back hurts 🤣 ");
-    } else {
-      document.write(" Go Party! ");
-    }
+  let Age = prompt("Are you over 30?");
+  let answer = Age.charAt(0).toLowerCase();
+  if( answer === "y" ) {
+    document.write(" I bet your back hurts 🤣 ");
+  } else {
+    document.write(" Go Party! ");
   }
-  getAge()
+}
 
 // Sneaker Question
 function getSneakers() {
-    let Sneaker = prompt("Are you a sneakerhead?");
-    let answer = Sneaker.charAt(0).toLowerCase();
-    if( answer === "y" ) {
-      document.write(" Awesome, so am I! ");
-    } else {
-      document.write(" I'll buy the pairs you don't. ");
-    }
+  let Sneaker = prompt("Are you a sneakerhead?");
+  let answer = Sneaker.charAt(0).toLowerCase();
+  if( answer === "y" ) {
+    document.write(" Awesome, so am I! ");
+  } else {
+    document.write(" I'll buy the pairs you don't. ");
   }
-  getSneakers()
+}
 
 // Pizza Pizza
 function getPizza() {
-    let pizza = prompt("I know you like pizza right?");
-    let answer = pizza.charAt(0).toLowerCase();
-    if( answer === "y" ) {
-      document.write(" Ohhh Yeahhh, my kind of friend. ");
-    } else {
-      document.write(" Ehh, guess you're ok.... ");
+  let pizza = prompt("I know you like pizza right?");
+  let answer = pizza.charAt(0).toLowerCase();
+  if( answer === "y" ) {
+    document.write(" Ohhh Yeahhh, my kind of friend. ");
+  } else {
+    document.write(" Ehh, guess you're ok.... ");
+  }
+}
+
+// Guessing game
+function guessTheNumber() {
+  let correctAnswer = false;
+  let numberOfGuesses = 3;
+
+  for( let i = 0; i < numberOfGuesses; i++) {
+    let guess = prompt("Guess the number I'm thinking from 1-10");
+    guess = Number(guess);
+    if( guess === 7 ) {
+      correctAnswer = true;
+      numberOfGuesses = i; 
+      break;
     }
   }
-  getPizza()
+
+  if( correctAnswer ) {
+    document.write(numberOfGuesses);
+  } else {
+    document.write("Soo Close! It's 7");
+  }
+}
+
+// Array question
+
+function favoriteColor() {
+  let favoriteColors = ["black", "navy blue", "military green"];
+  for ( let i = 1; i < 7; i++ ) {
+    let userGuess = prompt("What's my favorite color?");
+    if (favoriteColors.indexOf(userGuess.toLowerCase()) !== -1) {
+      alert("You know me well!");
+      break;
+    } else {
+      alert("Sorry, try once more.");
+    }
+
+    if( i > 7 ) {
+      document.write("Sorry you couldn't guess it, the color is" + favoriteColors);
+    }
+  }
+}
+favoriteColor();
 
